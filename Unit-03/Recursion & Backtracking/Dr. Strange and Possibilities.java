57633499d8d0b192574407835906b47887a52c39
Md Talib Ansari <93334743+1talib@users.noreply.github.com>
2022-04-25 14:54:40 +0530

    Update Dr. Strange and Possibilities.java

--- Unit-03/Recursion & Backtracking/Dr. Strange and Possibilities.java
+++ Unit-03/Recursion & Backtracking/Dr. Strange and Possibilities.java
@@ -51,3 +51,23 @@ class Main{
         }
     }
 }
+// import java.util.Scanner;
+// class Main {
+//     static void Possible(String Str, String[] arr, int start, int end){
+//         if (start <= end && !Str.equals(" ")){
+//             System.out.println(Str);
+//         }
+//         for (int i = start; i < end; i++){
+//             Possible(Str + arr[i] + " ", arr, i + 1, end);
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+//         String[] arr = new String[num];
+//         for (int i = 1; i <= num; i++) {
+//             arr[i - 1] = i + "";
+//         }
+//         Possible("", arr, 0, num);
+//     }
+// }
