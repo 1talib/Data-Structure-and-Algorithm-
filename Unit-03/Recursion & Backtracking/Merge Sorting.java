3ffae832eaa0ff3dbb433a206aafa0e82a73a72e
Md Talib Ansari <93334743+1talib@users.noreply.github.com>
2022-05-16 09:48:54 +0530

    Update Merge Sorting.java

--- Unit-03/Recursion & Backtracking/Merge Sorting.java
+++ Unit-03/Recursion & Backtracking/Merge Sorting.java
@@ -89,3 +89,67 @@ class Main{
     }
   }
 }
+// import java.lang.*;
+// import java.io.*;
+// import java.util.Scanner;
+// class Main
+// {
+//    public static  void merge(int arr[], int l, int m, int r)
+//     {
+//         int n1 = m - l + 1;
+//         int n2 = r - m;
+//         int L[] = new int[n1];
+//         int R[] = new int[n2];
+//         for (int i = 0; i < n1; ++i)
+//             L[i] = arr[l + i];
+//         for (int j = 0; j < n2; ++j)
+//             R[j] = arr[m + 1 + j];
+//         int i = 0, j = 0;
+//         int k = l;
+//         while (i < n1 && j < n2){
+//             if (L[i] <= R[j]){
+//                 arr[k] = L[i];
+//                 i++;
+//             } 
+//             else {
+//                 arr[k] = R[j];
+//                 j++;
+//             }
+//             k++;
+//         }
+//         while (i < n1) {
+//           arr[k] = L[i];
+//             i++;
+//             k++;
+//         }
+//         while (j < n2) {
+//             arr[k] = R[j];
+//             j++;
+//             k++;
+//         }
+//     }
+//    public static void mergeSort(int arr[], int l, int r)
+//     {
+//         if (l < r) {
+//             int m = l + (r-l)/2;
+//             mergeSort(arr, l, m);
+//             mergeSort(arr, m + 1, r);
+//             merge(arr, l, m, r);
+//         }
+//     }
+//     static void printArray(int arr[])
+//     {
+//         int n = arr.length;
+//         for (int i = 0; i < n; ++i)
+//         System.out.print(arr[i] + " ");
+//         System.out.println();
+//     }
+//     public static void main(String [] args){
+//     Scanner sc = new Scanner(System.in);
+//     int len = sc.nextInt();
+//     int[] arr = new int[len];
+//     for(int i=0; i<len; i++) arr[i] = sc.nextInt();
+//     mergeSort(arr,0,len-1);
+//     for(int i=0; i<arr.length; i++) System.out.print(arr[i]+" ");
+//   }
+// }   
