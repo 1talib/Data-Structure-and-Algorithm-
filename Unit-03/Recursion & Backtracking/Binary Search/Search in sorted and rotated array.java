06dccfd9e23ed5774f5638fe8a7cd98fcc3ef24c
Md Talib Ansari <93334743+1talib@users.noreply.github.com>
2022-05-16 10:49:30 +0530

    Update Search in sorted and rotated array.java

--- Unit-03/Recursion & Backtracking/Binary Search/Search in sorted and rotated array.java
+++ Unit-03/Recursion & Backtracking/Binary Search/Search in sorted and rotated array.java
@@ -92,3 +92,35 @@ else{
      }
    }
   }
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int k = sc.nextInt();
+//         int[] arr = new int[n];
+//         for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+//         System.out.println(res(n, arr, k));
+//       }
+//     static int res(int n, int[] arr, int k) {
+//         int low = 0, high = n - 1;
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             if (arr[mid] == k) return mid;
+//             else if (arr[low] <= arr[mid]) {
+//       //  low to mid is sorted
+//                 if (k >= arr[low] && k <= arr[mid])
+//                     high = mid - 1;
+//                 else
+//                     low = mid + 1;
+//             } else if (arr[mid] <= arr[high]) {
+//       //  mid to high is sorted
+//                 if (k > arr[mid] && arr[high] >= k)
+//                     low = mid + 1;
+//                 else
+//                     high = mid - 1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
