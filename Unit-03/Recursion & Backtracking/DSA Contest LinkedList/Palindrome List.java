39bec86e084d45af68fe85f84d2545de61090574
Md Talib Ansari <93334743+1talib@users.noreply.github.com>
2022-05-25 11:31:48 +0530

    Update Palindrome List.java

--- Unit-03/Recursion & Backtracking/DSA Contest LinkedList/Palindrome List.java
+++ Unit-03/Recursion & Backtracking/DSA Contest LinkedList/Palindrome List.java
@@ -50,27 +50,47 @@ class Node{
   }
 }
 */
-class Solution{
-  public static boolean detectPalindrome(Node a){
-    //complete this function.
-    
-    Node pre=null;
-    Node temp=a;
-    Node current=a;
-    Node future=null;
-    
-    while(temp!=null){
-      future=temp.next;
-      temp.next=pre;
-        pre=temp;
-      temp=future;
+class Solution { 
+    public static boolean check(List <Integer> list){
+        int k = list.size()-1; 
+        int i = 0; 
+        while (i <= k){ 
+        if (list.get (i) != list.get(k)) return false;
+        i++; 
+        k--; 
+          } 
+        return true;
+       } 
+    public static boolean detectPalindrome(Node a){
+        List<Integer>list = new ArrayList<>(); 
+        while (a != null){ 
+        list.add(a.data); 
+        a = a.next; 
+              } 
+        return check(list); 
+         }
     }
+// class Solution{
+//   public static boolean detectPalindrome(Node a){
+//     //complete this function.
     
-    while(current!=null){
-      if(current.data != pre.data) return false;
-      current=current.next;
-      pre=pre.next;
-    }
-    return true;
-  }
-}
+//     Node pre=null;
+//     Node temp=a;
+//     Node current=a;
+//     Node future=null;
+    
+//     while(temp!=null){
+//       future=temp.next;
+//       temp.next=pre;
+//         pre=temp;
+//       temp=future;
+//     }
+    
+//     while(current!=null){
+//       if(current.data != pre.data) return false;
+//       current=current.next;
+//       pre=pre.next;
+//     }
+//     return true;
+//   }
+// }
