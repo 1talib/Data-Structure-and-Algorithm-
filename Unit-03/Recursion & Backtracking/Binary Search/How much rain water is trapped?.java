c2270051133586f3687574d7531cdec097848d5f
Md Talib Ansari <93334743+1talib@users.noreply.github.com>
2022-05-29 07:54:06 +0530

    Create How much rain water is trapped?.java

---
+++ Unit-03/Recursion & Backtracking/Binary Search/How much rain water is trapped?.java
@@ -0,0 +1,39 @@
+/*
+Description
+
+Givennnon-negative integers representing an elevation map where the width of each bar is 1, calculate how much rain water will be trapped after it rains.
+
+
+Input
+Input Format
+
+First line contains T the number of test cases
+
+Second line contains N the size of the array
+
+Third line contains N space separated integers
+
+Constraints
+
+1 <= T <= 10
+
+1 <= N <= 10e5
+
+0 <= a[i] <= 1000
+
+
+Output
+Print a single integer, the amount of rain water trapped
+
+
+Sample Input 1 
+
+2
+5
+3 2 0 4 6
+7
+7 0 3 6 2 3 5
+Sample Output 1
+
+4
+14*/
