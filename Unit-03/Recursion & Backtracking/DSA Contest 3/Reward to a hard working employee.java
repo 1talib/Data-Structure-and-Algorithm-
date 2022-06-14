e27938412879c144bae47bacc4e6a0d262c0cec5
Md Talib Ansari <93334743+1talib@users.noreply.github.com>
2022-06-14 14:26:47 +0530

    Update Reward to a hard working employee.java

--- Unit-03/Recursion & Backtracking/DSA Contest 3/Reward to a hard working employee.java
+++ Unit-03/Recursion & Backtracking/DSA Contest 3/Reward to a hard working employee.java
@@ -48,4 +48,53 @@ Explanation to Sample test case:
 The first truck carries the first box and then return to the house in 2 hours again carries the 2nd box and return and again the 3rd box and return taking a total of 6 hours.
 
 The second truck similarly carries the next 3 boxes and return to the house in 6 hours. In the 6th hour, one of the two truck gets loaded with the 7th box and reaches Town X with the last box at the 7th hour.*/
-  
+ import java.util.*;
+class Main{
+    public static boolean isPossible( int time, int[] arr, int[] cap){
+        int boxesInOneRound = time/2;
+        if(time%2!=0){
+            boxesInOneRound++;
+        }
+        int j = arr.length-1;
+        for(int i = cap.length-1; i>=0;i--){
+            int count = 0;
+            while(j>=0 && count<boxesInOneRound){
+                if (cap[i] >= arr[j]){
+                    count++;
+                }else{
+                    return false;
+                }
+                j--;
+            }
+        }
+        if (j>=0)
+        return false;
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[]arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int[]cap = new int[m];
+        for(int i = 0; i<m; i++){
+            cap[i] = sc.nextInt();
+        }
+        Arrays.sort(arr); Arrays.sort(cap);
+        int l=1, r=(int)1e8;
+        int ans = -1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(isPossible(mid, arr, cap) == true){
+                ans = mid;
+                r = mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
